f2891e89955a654b45682be9eebe08690564d32a
Denis <145012169+orangutangent@users.noreply.github.com>
2024-10-29 01:22:13 +0700

    updated next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/star-map-landing",
+  assetPrefix: "/star-map-landing",
+  publicRuntimeConfig: {
+    basePath: "/star-map-landing",
+  },
 };
 
 export default nextConfig;
